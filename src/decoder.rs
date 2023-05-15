a184ce734cb3072f3d81d429ca584b0577a97af2
Willem Verstraeten <willem.verstraeten@gmail.com>
2023-05-15 21:55:19 +0200

    cleanup

--- src/decoder.rs
+++ src/decoder.rs
@@ -8,39 +8,37 @@ pub struct Decoder {
 
 impl Decoder {
     pub fn new() -> Decoder {
-        let mut l = OpDecoderLookup::new();
-        l.insert("0b1000_10dw", MovToFromRegMemDecoder {});
-        l.insert("0b1100_011w", ImmediateMovToRegMemDecoder {});
-        l.insert("0b1011_wreg", ImmediateMovToRegDecoder {});
-        l.insert("0b1010_00dw", MovAccumulatorDecoder {});
-        l.insert("0b1000_11d0", MovSegmentDecoder {});
+        let mut lookup = OpDecoderLookup::new();
+        lookup.insert("0b1000_10dw", MovToFromRegMemDecoder {});
+        lookup.insert("0b1100_011w", ImmediateMovToRegMemDecoder {});
+        lookup.insert("0b1011_wreg", ImmediateMovToRegDecoder {});
+        lookup.insert("0b1010_00dw", MovAccumulatorDecoder {});
+        lookup.insert("0b1000_11d0", MovSegmentDecoder {});
 
-        l.insert("0b00xx_x0dw", ArithmeticFromToRegMemDecoder {});
-        l.insert("0b1000_00sw", ArithmeticImmediateToRegMemDecoder {});
-        l.insert("0b00xx_x1dw", ArithmeticImmediateToAccumulatorDecoder {});
+        lookup.insert("0b00xx_x0dw", ArithmeticFromToRegMemDecoder {});
+        lookup.insert("0b1000_00sw", ArithmeticImmediateToRegMemDecoder {});
+        lookup.insert("0b00xx_x1dw", ArithmeticImmediateToAccumulatorDecoder {});
 
-        l.insert("0b0111_0100", JumpDecoder::new(Instruction::JumpOnEqual));
-        l.insert("0b0111_1100", JumpDecoder::new(Instruction::JumpOnLess));
-        l.insert("0b0111_1110", JumpDecoder::new(Instruction::JumpOnNotGreater));
-        l.insert("0b0111_0010", JumpDecoder::new(Instruction::JumpOnBelow));
-        l.insert("0b0111_0110", JumpDecoder::new(Instruction::JumpOnNotAbove));
-        l.insert("0b0111_1010", JumpDecoder::new(Instruction::JumpOnParity));
-        l.insert("0b0111_0000", JumpDecoder::new(Instruction::JumpOnOverflow));
-        l.insert("0b0111_1000", JumpDecoder::new(Instruction::JumpOnSign));
-        l.insert("0b0111_0101", JumpDecoder::new(Instruction::JumpOnNotEqual));
-        l.insert("0b0111_1101", JumpDecoder::new(Instruction::JumpOnNotLess));
-        l.insert("0b0111_1111", JumpDecoder::new(Instruction::JumpOnGreater));
-        l.insert("0b0111_0011", JumpDecoder::new(Instruction::JumpOnNotBelow));
-        l.insert("0b0111_0111", JumpDecoder::new(Instruction::JumpOnAbove));
-        l.insert("0b0111_1011", JumpDecoder::new(Instruction::JumpOnNoParity));
-        l.insert("0b0111_0001", JumpDecoder::new(Instruction::JumpOnNoOverflow));
-        l.insert("0b0111_1001", JumpDecoder::new(Instruction::JumpOnNotSign));
-        l.insert("0b1110_0010", JumpDecoder::new(Instruction::Loop));
-        l.insert("0b1110_0001", JumpDecoder::new(Instruction::LoopWhileEqual));
-        l.insert("0b1110_0000", JumpDecoder::new(Instruction::LoopWhileNotEqual));
-        l.insert("0b1110_0011", JumpDecoder::new(Instruction::JumpOnCxZero));
-
-        let lookup = l;
+        lookup.insert("0b0111_0100", JumpDecoder::new(Instruction::JumpOnEqual));
+        lookup.insert("0b0111_1100", JumpDecoder::new(Instruction::JumpOnLess));
+        lookup.insert("0b0111_1110", JumpDecoder::new(Instruction::JumpOnNotGreater));
+        lookup.insert("0b0111_0010", JumpDecoder::new(Instruction::JumpOnBelow));
+        lookup.insert("0b0111_0110", JumpDecoder::new(Instruction::JumpOnNotAbove));
+        lookup.insert("0b0111_1010", JumpDecoder::new(Instruction::JumpOnParity));
+        lookup.insert("0b0111_0000", JumpDecoder::new(Instruction::JumpOnOverflow));
+        lookup.insert("0b0111_1000", JumpDecoder::new(Instruction::JumpOnSign));
+        lookup.insert("0b0111_0101", JumpDecoder::new(Instruction::JumpOnNotEqual));
+        lookup.insert("0b0111_1101", JumpDecoder::new(Instruction::JumpOnNotLess));
+        lookup.insert("0b0111_1111", JumpDecoder::new(Instruction::JumpOnGreater));
+        lookup.insert("0b0111_0011", JumpDecoder::new(Instruction::JumpOnNotBelow));
+        lookup.insert("0b0111_0111", JumpDecoder::new(Instruction::JumpOnAbove));
+        lookup.insert("0b0111_1011", JumpDecoder::new(Instruction::JumpOnNoParity));
+        lookup.insert("0b0111_0001", JumpDecoder::new(Instruction::JumpOnNoOverflow));
+        lookup.insert("0b0111_1001", JumpDecoder::new(Instruction::JumpOnNotSign));
+        lookup.insert("0b1110_0010", JumpDecoder::new(Instruction::Loop));
+        lookup.insert("0b1110_0001", JumpDecoder::new(Instruction::LoopWhileEqual));
+        lookup.insert("0b1110_0000", JumpDecoder::new(Instruction::LoopWhileNotEqual));
+        lookup.insert("0b1110_0011", JumpDecoder::new(Instruction::JumpOnCxZero));
 
         Decoder { lookup }
     }
